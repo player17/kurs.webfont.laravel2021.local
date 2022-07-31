9032ae3ae5324c9d20425139004544102f2883a6
reandh <reandh@yandex.ru>
2022-08-01 03:54:10 +0500

    Lesson 7 - Controllers

---
+++ 01-baseLaravel/app/Http/Controllers/PageController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class PageController extends Controller
+{
+    public function show($slug) {
+        return view("pages.show", ['slug' => $slug]);
+    }
+}
--- 01-baseLaravel/routes/web.php
+++ 01-baseLaravel/routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// '/'
+/*
 Route::get('/', function () {
     $res = 555;
     $name = 'My name';
@@ -20,9 +22,22 @@ Route::get('/', function () {
     //return view('home',['res' => $res, 'name' => $name]);
     return view('home', compact('res', 'name'));
 })->name('home');
+*/
+
+// '/test'
+// Route::view('/test', 'test', ['test' => 'test data']);
+
+// '/about'
+// Route::redirect('/about', '/contact', 301);
+
+
+Route::get('/', 'HomeController@index');
+Route::get('/test', 'HomeController@test');
+Route::get('/test2', 'Test\TestController@index');
+Route::get('/page/{slug}', 'PageController@show', 301);
 
 Route::get('/about', function () {
-   return 'page about';
+    return 'page about';
 });
 
 /*
@@ -38,33 +53,29 @@ Route::post('/send-email', function (){
 });
 */
 //Route::any();
-Route::match(['post', 'get', 'put'], '/contact', function (){
-    if(!empty($_POST)) {
+Route::match(['post', 'get', 'put'], '/contact', function () {
+    if (!empty($_POST)) {
         dump($_POST);
     }
     return view('contact');
 })->name('contact-route');
 
-Route::view('/test', 'test', ['test' => 'test data']);
-
-Route::redirect('/about', '/contact', 301);
-
 // Route::get('post/{id}/{slug}', function ($id, $slug){return "Post $id : $slug";})->where(['id' => '[0-9]+', 'slug' => '[A-Za-z0-9-]+']);
 // /app/Providers/RouteServiceProvider.php::public function boot()
-Route::get('post/{id}/{slug?}', function ($id, $slug = null){
+Route::get('post/{id}/{slug?}', function ($id, $slug = null) {
     return "Post $id : $slug";
 })->name('post');
 
-Route::prefix('admin')->name('admin.')->group(function (){
-    Route::get('/posts',function (){
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('/posts', function () {
         return 'Posts List';
     });
 
-    Route::get('/post/create',function (){
+    Route::get('/post/create', function () {
         return 'Post Create';
     });
 
-    Route::get('/post/{id}/edit',function ($id){
+    Route::get('/post/{id}/edit', function ($id) {
         return "Edit Posts $id";
     })->name('post');
 });
