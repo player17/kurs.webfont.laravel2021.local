8463c89269d96e0d4f8d12fc11a3ffba9569242f
reandh <reandh@yandex.ru>
2022-08-03 20:36:21 +0500

    01 Lesson 09 - Get Config App

--- 01-baseLaravel/app/Http/Controllers/HomeController.php
+++ 01-baseLaravel/app/Http/Controllers/HomeController.php
@@ -6,6 +6,9 @@ class HomeController extends Controller
 {
     public function index()
     {
+        dump(config('app.timezone'));
+        dump(config('database.connections.mysql'));
+        dump($_ENV);
         return view('home', ['res' => 5, 'name' => 'super']);
     }
 
