23fd001ebff7e3ea391cd29531cf45b97f60a1be
reandh <reandh@yandex.ru>
2022-08-12 20:55:59 +0500

    02 Lesson 2 - Install AdminLTE3

--- 02-baseLaravel/webpack.mix.js
+++ 02-baseLaravel/webpack.mix.js
@@ -11,5 +11,32 @@ const mix = require('laravel-mix');
  |
  */
 
+/*
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+ */
+
+mix.styles([
+    'resources/assets/admin/plugins/fontawesome-free/css/all.css',
+    'resources/assets/admin/css/adminlte.css'
+], 'public/assets/admin/css/admin.css');
+
+//mix.postCss('public/assets/admin/css/admin.css', 'public/assets/admin/css');
+
+mix.scripts([
+    'resources/assets/admin/plugins/jquery/jquery.js',
+    'resources/assets/admin/plugins/bootstrap/js/bootstrap.bundle.js',
+    'resources/assets/admin/js/adminlte.js',
+    'resources/assets/admin/js/demo.js'
+], 'public/assets/admin/js/admin.js');
+
+mix.copyDirectory('resources/assets/admin/plugins/fontawesome-free/webfonts', 'public/assets/admin/webfonts');
+mix.copyDirectory('resources/assets/admin/img', 'public/assets/admin/img');
+
+mix.copy('resources/assets/admin/js/adminlte.js.map', 'public/assets/admin/js/adminlte.js.map');
+mix.copy('resources/assets/admin/css/adminlte.css.map', 'public/assets/admin/css/adminlte.css.map');
+
+// `npm install`
+// `npm run dev` // не сжимает
+// `npm run production` // c;bvftn
+
