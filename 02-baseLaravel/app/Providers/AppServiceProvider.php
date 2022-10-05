f88b150aeffc2e84ad55fabc965ae8b12730e88c
reandh <reandh@yandex.ru>
2022-10-05 18:23:57 +0500

    02 Lesson 21 - Sidebar && AppServiceProvider && Cache

--- 02-baseLaravel/app/Providers/AppServiceProvider.php
+++ 02-baseLaravel/app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Category;
+use App\Post;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +26,25 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Получение данных для sidebar
+        view()->composer('layouts.sidebar', function($view) {
+
+            // Кеширование категорий
+            if(Cache::has('cats')) {
+                $cats = Cache::get('cats');
+            } else {
+                // https://laravel.com/docs/9.x/eloquent-relationships
+                // select `categories`.*, (select count(*) from `posts` where `categories`.`id` = `posts`.`category_id`) as `posts_count` from `categories` order by `posts_count` desc
+                $cats = Category::withCount('posts')->orderBy('posts_count', 'desc')->get();
+                // Кладем в кеш на 30 секунд
+                Cache::put('cats', $cats, 30);
+            }
+
+            // Популярные посты
+            $view->with('popular_posts', Post::orderBy('views', 'desc')->limit(3)->get());
+
+            // Список категорий + количество постов
+            $view->with('cats', $cats);
+        });
     }
 }
